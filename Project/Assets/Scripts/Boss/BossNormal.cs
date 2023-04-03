626b2d0c39af405e8a138e6c42f1d5a5ba8b1d71
kimhjong123 <128135992+kimhjong123@users.noreply.github.com>
2023-04-03 10:05:29 +0900

    Update BossNormal.cs

--- Project/Assets/Scripts/Boss/BossNormal.cs
+++ Project/Assets/Scripts/Boss/BossNormal.cs
@@ -4,23 +4,19 @@ using UnityEngine;
 
 public class BossNormal : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject, 5f);
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)   // 플레이어와 충돌 시 플레이어 Hp감소
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            //GameObject.Find("Player").GetComponent<Player>().Hp -= 1;
             float dmg = 1;
             EntityEffectController.eec.CreateDamageText(GameManager.gm.player, dmg);
             other.gameObject.GetComponent<Player>().Hp -= (int)dmg;
             Destroy(gameObject);
         }
     }
-
-    // Update is called once per frame
 }
