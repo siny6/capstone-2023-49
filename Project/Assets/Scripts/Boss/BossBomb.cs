1ff1b79ed1844cc7d052032b301a5b913b7af8be
kimhjong123 <128135992+kimhjong123@users.noreply.github.com>
2023-04-03 10:01:05 +0900

    Update BossBomb.cs

--- Project/Assets/Scripts/Boss/BossBomb.cs
+++ Project/Assets/Scripts/Boss/BossBomb.cs
@@ -5,24 +5,23 @@ using UnityEngine;
 public class BossBomb : MonoBehaviour
 {
     Animator animator;
-    // Start is called before the first frame update
+    
     void Start()
     {
-        animator = GetComponent<Animator>();  // bomb �ִϸ��̼� ����ϱ� ���ؼ� �ʿ�
+        animator = GetComponent<Animator>(); 
         Invoke("bomb", 3f);
     }
     
     void bomb()
     {
-        animator.SetTrigger("bomb");          // bomb �ִϸ��̼� �ߵ� Ʈ����
+        animator.SetTrigger("bomb");          // bomb 애니메이션 시작
         Destroy(gameObject, 2f);
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)  // 플레이어와 충돌시 데미지 입힘, 데미지 띄우기
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            //GameObject.Find("Player").GetComponent<Player>().Hp -= 1;
             float dmg = 1;
             EntityEffectController.eec.CreateDamageText(GameManager.gm.player, dmg);
             other.gameObject.GetComponent<Player>().Hp -= (int)dmg;
